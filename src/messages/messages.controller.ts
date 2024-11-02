b6f905bfc28de2a2338e0630dfb90ed48a49864f
tilo1306 <doug1306@gmail.com>
2024-11-01 21:37:58 -0300

    feat: rota patch

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,17 +1,38 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
   @Get()
+  @HttpCode(HttpStatus.OK)
   findAll(): string {
     return 'Essa rota retorna todos os recados';
   }
   @Get(':id')
+  @HttpCode(HttpStatus.OK)
   findOne(@Param('id') id: string): string {
     return `Essa rota retorna o recado ${id}`;
   }
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   created(@Body() body: any) {
     return body;
+  }
+
+  @Patch(':id')
+  @HttpCode(HttpStatus.OK)
+  update(@Param('id') id: string, @Body() body: any) {
+    return {
+      id,
+      ...body,
+    };
   }
 }
